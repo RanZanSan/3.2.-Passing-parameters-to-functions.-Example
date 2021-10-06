ec199012fd291837fc64085656a9204114052271
Andrey Rasskazov <ranzansan@gmail.com>
2021-10-06 14:49:27 +0300

    Добавьте файлы проекта.

---
+++ exampleFunctions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exampleFunctions
+{
+    class Program
+    {
+        static void Print(int[] values)
+        {
+            int length = values.Length;
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(values[i] + " ");
+            }
+        }
+
+        static void Change(int[] array)
+        {
+            int len = array.Length;
+            for (int i = 0; i < len; i++)
+            {
+                array[i] = array[i] * 2;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 1, 4, 5, 7 };
+            Print(a);
+            Change(a);
+            Print(a);
+            Console.WriteLine("Конец Main...");
+        }
+    }
+}
